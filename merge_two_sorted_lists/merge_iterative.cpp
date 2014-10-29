aaaeefb1d883804bf7c40dfb3719332b49c0afe5
xbz <xbz.o3sig@gmail.com>
2014-10-29 18:59:26 +0800

    add merge_two_sorted_list
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

---
+++ merge_two_sorted_lists/merge_iterative.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *ret = new ListNode(0);
+        ListNode *p = ret;
+        while (l1!=NULL && l2!=NULL) {
+            ListNode *cur = new ListNode(0);
+            if (l1->val <= l2->val) {
+                cur->val = l1->val;
+                l1 = l1->next;
+            } else {
+                cur->val = l2->val;
+                l2 = l2->next;
+            };
+            p->next = cur;
+            p = cur;
+        }
+        while (l1 != NULL) {
+            p->next = new ListNode(l1->val);
+            p = p->next;
+            l1 = l1->next;
+        }
+        while (l2 != NULL) {
+            p->next = new ListNode(l2->val);
+            p = p->next;
+            l2 = l2->next;
+        }
+        return ret->next;
+    }
+};
+
+void print(ListNode *p) {
+    while (p != NULL) {
+        cout << p->val;
+        p = p->next;
+        if (p != NULL)
+            cout << "->";
+    }
+    cout << endl;
+}
+
+void addNode(ListNode *p, int value) {
+    if (p == NULL)
+        return;
+
+    ListNode *pCur = new ListNode(value);
+    while (p != NULL) {
+        if (p->next == NULL)
+            break;
+        p = p->next;
+    }
+    p->next = pCur;
+}
+
+// sumbit times(for the first accept): 1
+int main(int argc, char *argv[])
+{
+    Solution s;
+    ListNode *l1 = new ListNode(180);
+    addNode(l1, 280);
+    ListNode *l2 = new ListNode(100);
+    addNode(l2, 150);
+    addNode(l2, 200);
+    addNode(l2, 250);
+    addNode(l2, 300);
+    ListNode *p = s.mergeTwoLists(l1, l2);
+    print(p);
+
+    return 0;
+}
---
+++ merge_two_sorted_lists/merge_recursive.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+        ListNode *p = new ListNode(0);
+        if (l1->val <= l2->val) {
+            p->val = l1->val;
+            p->next = mergeTwoLists(l1->next, l2);
+        } else {
+            p->val = l2->val;
+            p->next = mergeTwoLists(l1, l2->next);
+        }
+        return p;
+    }
+};
+
+void print(ListNode *p) {
+    while (p != NULL) {
+        cout << p->val;
+        p = p->next;
+        if (p != NULL)
+            cout << "->";
+    }
+    cout << endl;
+}
+
+void addNode(ListNode *p, int value) {
+    if (p == NULL)
+        return;
+
+    ListNode *pCur = new ListNode(value);
+    while (p != NULL) {
+        if (p->next == NULL)
+            break;
+        p = p->next;
+    }
+    p->next = pCur;
+}
+
+// sumbit times(for the first accept): 1
+int main(int argc, char *argv[])
+{
+    Solution s;
+    ListNode *l1 = new ListNode(100);
+    addNode(l1, 220);
+    ListNode *l2 = new ListNode(100);
+    addNode(l2, 150);
+    addNode(l2, 200);
+    addNode(l2, 250);
+    addNode(l2, 300);
+    ListNode *p = s.mergeTwoLists(l1, l2);
+    print(p);
+
+    return 0;
+}
