b13e342fb220167be2f0c8d0bc9a4c68bc53cadf
xbz <xbz.o3sig@gmail.com>
2014-11-16 09:46:33 +0800

    validate bst
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

---
+++ validate_binary_search_tree/validate.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void print(const vector<int>& v)
+{
+    vector<int>::size_type i;
+    for (i=0; i<v.size()-1; i++)
+        cout << v[i] << ' ';
+    cout << v[i] << endl;
+}
+
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        if (!root)
+            return true;
+
+        vector<int> seq;
+        stack<TreeNode *> s;
+        TreeNode *cur = root;
+        while (1) {
+            while (cur) {
+                s.push(cur);
+                cur = cur->left;
+            }
+            if (s.empty())
+                break;
+            cur = s.top();
+            seq.push_back(cur->val);
+            s.pop();
+            cur = cur->right;
+        }
+        for (vector<int>::size_type i=0; i<seq.size()-1; ++i)
+            if (seq[i] >= seq[i+1])
+                return false;
+        return true;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    // TreeNode *empty = NULL;
+    TreeNode *t1 = new TreeNode(5);
+    TreeNode *t2 = new TreeNode(50);
+    TreeNode *t3 = new TreeNode(15);
+    t3->left = t1;
+    t3->right = t2;
+    TreeNode *t4 = new TreeNode(50);
+    TreeNode *t5 = new TreeNode(20);
+    t5->left = t3;
+    t5->right = t4;
+    TreeNode *t6 = new TreeNode(12);
+    t2->left = t6;
+    TreeNode *t7 = new TreeNode(6);
+    t1->right = t7;
+    Solution s;
+
+    bool isBST = s.isValidBST(t3);
+    cout << "tree is bst:" << isBST << endl;
+
+    return 0;
+}
