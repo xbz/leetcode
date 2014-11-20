a554ffc2e5d246ce49248fc2f40cdce5341d837d
xbz <xbz.o3sig@gmail.com>
2014-11-20 17:06:15 +0800

    add list cycle
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

---
+++ linked_list_cycle/list_cycle.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void print(const ListNode *head)
+{
+    while (head) {
+        cout << head->val << " ";
+        head =  head->next;
+    }
+}
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head)
+            return false;
+        set<ListNode *> s;
+        ListNode *p = head;
+        while (p) {
+            if (s.find(p) != s.end())
+                return true;
+            s.insert(p);
+            p = p->next;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Solution s;
+    ListNode *head = new ListNode(10);
+    ListNode *p = head;
+    p->next = new ListNode(50);
+    p = p->next;
+    ListNode *q = p;
+    p->next = new ListNode(70);
+    p = p->next;
+    // p->next = q;
+    print(head);
+
+    bool bCycle = s.hasCycle(head);
+    cout << "has cycle:" << bCycle << endl;
+
+    return 0;
+}
