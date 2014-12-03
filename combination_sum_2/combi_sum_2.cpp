d5c266bdae99031090a61f68c39b1f7bf02ea5d3
xbz <xbz.o3sig@gmail.com>
2014-12-03 10:45:31 +0800

    add combination sum 2
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

---
+++ combination_sum_2/combi_sum_2.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(const vector<int>& v)
+{
+    vector<int>::const_iterator it;
+    for (it=v.begin(); it!=v.end(); ++it)
+        cout << *it;
+    cout << endl;
+}
+
+class Solution {
+public:
+    vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > ret;
+        vector<int> path;
+        dfs(ret, candidates, path, target);
+        return ret;
+    }
+
+private:
+    // brute force, not elegent
+    void dfs(vector<vector<int> > &ret, vector<int> &v, vector<int> &path,
+             int target) {
+        if (target == 0) {
+            if (path.size()>0) {
+                if (find(ret.begin(), ret.end(), path) == ret.end())
+                    ret.push_back(path);
+            }
+            return;
+        }
+
+        for (size_t i=0; i!=v.size(); ++i) {
+            if (v[i] > target)
+                continue;
+
+            path.push_back(v[i]);
+            vector<int> part(v.begin()+i+1, v.end());
+            dfs(ret, part, path, target-v[i]);
+            path.pop_back();
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Solution s;
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(6);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(1);
+    v.push_back(1);
+    //v.push_back(5);
+
+    int target = 8;
+    vector<vector<int> > ret = s.combinationSum2(v, target);
+    cout << "size:" << ret.size() << endl;
+    vector<vector<int> >::iterator it;
+    for (it=ret.begin(); it!=ret.end(); ++it)
+        print(*it);
+
+    return 0;
+}
+
