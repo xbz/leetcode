634f82c8147bae8bcfcdac4b25e56396353b7309
xbz <xbz.o3sig@gmail.com>
2015-01-12 15:15:43 +0800

    add unique-paths
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- unique_paths/path.cpp
+++ unique_paths/path.cpp
@@ -8,21 +8,19 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int **f = new int* [m];
-        for (int k=0; k<m; ++k) {
-            f[k] = new int[n];
-            memset(f[k], 0, n);
-        }
+        int f[m][n];
 
         f[0][0] = 1;
         for (int i=0; i<m; ++i) {
             for (int j=0; j<n; ++j) {
                 if (i==0 && j==0)
                     continue;
-                if (i!=0)
-                    f[i][j] += f[i-1][j];
-                if (j!=0)
-                    f[i][j] += f[i][j-1];
+                if (i == 0)
+                    f[i][j] = f[i][j-1];
+                else if (j == 0)
+                    f[i][j] = f[i-1][j];
+                else
+                    f[i][j] = f[i][j-1] + f[i-1][j];
             }
         }
         return f[m-1][n-1];
