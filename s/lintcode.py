d8dfdb0e4b0f833bc4ff402e933933d421c3e849
xbz <xbz.o3sig@gmail.com>
2015-01-19 17:22:34 +0800

    add minimum-subarray
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- s/lintcode.py
+++ s/lintcode.py
@@ -4,13 +4,20 @@ import urllib2
 from lxml import etree
 
 def main():
+    black = [
+        'maximum-subarray-iii',
+    ]
     resp = urllib2.urlopen('http://www.lintcode.com/en/daily/')
     html = resp.read()
 
     tree = etree.HTML(html)
     nodes = tree.xpath("id('problem-list')/div/a/@href")
     for p in nodes:
-        print p[9:]
+        str = p[9:]
+        if str in black:
+            continue
+
+        print str
 
 if __name__ == '__main__':
     main()
