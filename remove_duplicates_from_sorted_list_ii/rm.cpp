ee4b2c85c5ad651edc0f170ccf98c453cb5c76aa
xbz <xbz.o3sig@gmail.com>
2015-02-26 14:22:36 +0800

    refactor
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- remove_duplicates_from_sorted_list_ii/rm.cpp
+++ remove_duplicates_from_sorted_list_ii/rm.cpp
@@ -1,76 +1,145 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode(int x) : val(x), next(NULL) {}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
-  ListNode *deleteDuplicates(ListNode *head) {
-    if (head == NULL)
-      return head;
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *pre = &dummy;
 
-    ListNode *pp = new ListNode(0);
-    pp->next = head;
-    ListNode *prehead = pp;
-    bool dup = false;
-    while (head->next) {
-      if (head->val == head->next->val) {
-        ListNode *tmp = head;
-        head = head->next;
-        delete tmp;
-        dup = true;
-      } else {
-        if (dup)
-          pp->next = head->next;
-        else
-          pp = pp->next;
-        dup = false;
-        head = head->next;
-      }
+        while (pre->next!=NULL && pre->next->next!=NULL) {
+            if (pre->next->val == pre->next->next->val) {
+                int val = pre->next->val;
+                while (pre->next!=NULL && pre->next->val==val)
+                    pre->next = pre->next->next;
+            } else
+                pre = pre->next;
+        }
+        return dummy.next;
     }
-    if (dup)
-      pp->next = NULL;
-    return prehead->next;
-  }
+
+    ListNode *deleteDuplicates_cur_pre(ListNode *head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *pre = &dummy;
+        ListNode *cur = head;
+        while (cur != NULL) {
+            while (cur->next!=NULL && cur->val==cur->next->val)
+                cur = cur->next;
+            if (pre->next == cur)
+                pre = cur;
+            else
+                pre->next = cur->next;
+            cur = cur->next;
+        }
+        return dummy.next;
+    }
+
+    ListNode *deleteDuplicates_orig(ListNode *head) {
+        if (head == NULL)
+            return head;
+
+        ListNode *pp = new ListNode(0);
+        pp->next = head;
+        ListNode *prehead = pp;
+        bool dup = false;
+        while (head->next) {
+            if (head->val == head->next->val) {
+                ListNode *tmp = head;
+                head = head->next;
+                delete tmp;
+                dup = true;
+            } else {
+                if (dup)
+                    pp->next = head->next;
+                else
+                    pp = pp->next;
+                dup = false;
+                head = head->next;
+            }
+        }
+        if (dup)
+            pp->next = NULL;
+        return prehead->next;
+    }
+
+    ListNode *deleteDuplicates_direct(ListNode *head) {
+        ListNode *pre = new ListNode(0);
+        pre->next = head;
+        ListNode *ret = pre;
+        ListNode *cur = head;
+        while (cur) {
+            while (cur->next!=NULL && cur->val==cur->next->val)
+                cur = cur->next;
+            if (pre->next == cur)
+                pre = cur;
+            else
+                pre->next = cur->next;
+            cur = cur->next;
+        }
+        return ret->next;
+    }
+
+    ListNode *deleteDuplicates_map(ListNode *head) {
+        map<int, int> m;
+        while (head) {
+            m[head->val]++;
+            head = head->next;
+        }
+
+        ListNode *p = new ListNode(0);
+        ListNode *newhead = p;
+        for (map<int, int>::iterator it=m.begin(); it!=m.end(); ++it) {
+            if (it->second > 1)
+                continue;
+            p->next = new ListNode(it->first);
+            p = p->next;
+        }
+        return newhead->next;
+    }
 };
 
 void print_list(ListNode *head) {
-  while (head) {
-      cout << head->val << " ";
-      head = head->next;
-  }
-  cout << endl;
+    while (head) {
+            cout << head->val << " ";
+            head = head->next;
+    }
+    cout << endl;
 }
 
 ListNode *build_list(const vector<int>& v) {
-  ListNode *p = new ListNode(0);
-  ListNode *head = p;
-  for (size_t i=0; i<v.size(); i++) {
-    p->next = new ListNode(v[i]);
-    p = p->next;
-  }
-  return head->next;
+    ListNode *p = new ListNode(0);
+    ListNode *head = p;
+    for (size_t i=0; i<v.size(); i++) {
+        p->next = new ListNode(v[i]);
+        p = p->next;
+    }
+    return head->next;
 }
 
 int main(int argc, char **argv)
 {
-  int arr[] = {1, 2, 2, 4, 6, 6, 8};
-  //int arr[] = {2, 2, 4, 6, 6, 8};
-  int len = sizeof(arr) / sizeof(arr[0]);
-  vector<int> v(arr, arr+len);
-  ListNode *p = build_list(v);
+    int arr[] = {1, 2, 2, 4, 6, 6, 8};
+    //int arr[] = {2, 2, 4, 6, 6, 8};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    vector<int> v(arr, arr+len);
+    ListNode *p = build_list(v);
 
-  print_list(p);
-  Solution sol;
-  ListNode *new_list = sol.deleteDuplicates(p);
-  print_list(new_list);
+    print_list(p);
+    Solution sol;
+    ListNode *new_list = sol.deleteDuplicates(p);
+    print_list(new_list);
 
-  return 0;
+    return 0;
 }
 
