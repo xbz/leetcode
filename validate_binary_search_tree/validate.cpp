54433a18e36f122c6890322868a28a3d21b337ca
xbz <xbz.o3sig@gmail.com>
2015-03-11 11:56:17 +0800

    validate_binary_search_tree: add 2 solutions. 1.inorder traversal+pre, 2.low,high limit
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- validate_binary_search_tree/validate.cpp
+++ validate_binary_search_tree/validate.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <climits>
 
 using namespace std;
 
@@ -22,6 +23,40 @@ void print(const vector<int>& v)
 class Solution {
 public:
     bool isValidBST(TreeNode *root) {
+		return valid(root, LONG_MIN, LONG_MAX);
+	}
+
+	bool valid(TreeNode *root, long low, long high) {
+		if (root == NULL)
+			return true;
+		if (root->val<=low || root->val>=high)
+			return false;
+		return valid(root->left, low, root->val) && valid(root->right, root->val, high);
+	}
+
+    bool isValidBST_inorder(TreeNode *root) {
+		stack<TreeNode *> s;
+		TreeNode *pre = NULL;
+		TreeNode *cur = root;
+		while (true) {
+			while (cur != NULL) {
+				s.push(cur);
+				cur = cur->left;
+			}
+			if (s.empty())
+				break;
+
+			cur = s.top();
+			s.pop();
+			if (pre!=NULL && pre->val>=cur->val)
+				return false;
+			pre = cur;
+			cur = cur->right;
+		}
+		return true;
+	}
+
+    bool isValidBST_orig(TreeNode *root) {
         if (!root)
             return true;
 
@@ -45,6 +80,17 @@ public:
                 return false;
         return true;
     }
+
+    bool isValidBST_wrong(TreeNode *root) {
+        if (root == NULL)
+            return true;
+        if (root->left && root->left->val >= root->val)
+            return false;
+        if (root->right && root->right->val <= root->val)
+            return false;
+
+        return isValidBST(root->left) && isValidBST(root->right);
+    }
 };
 
 int main(int argc, char *argv[])
@@ -59,9 +105,9 @@ int main(int argc, char *argv[])
     TreeNode *t5 = new TreeNode(20);
     t5->left = t3;
     t5->right = t4;
-    TreeNode *t6 = new TreeNode(12);
+    TreeNode *t6 = new TreeNode(30);
     t2->left = t6;
-    TreeNode *t7 = new TreeNode(6);
+    TreeNode *t7 = new TreeNode(10);
     t1->right = t7;
     Solution s;
 
