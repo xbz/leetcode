541d2961ae073877c0ac8aea32269afbf9264927
xbz <xbz.o3sig@gmail.com>
2015-03-19 15:35:59 +0800

    complete longest-consecutive-sequence
    Signed-off-by: xbz <xbz.o3sig@gmail.com>

--- longest_consecutive_sequence/seq.cpp
+++ longest_consecutive_sequence/seq.cpp
@@ -15,67 +15,104 @@ void print(const unordered_map<int, int>& m)
 
 class Solution {
 public:
-    int longestConsecutive_better(vector<int> &num) {
-		int seq = 0;
+    int longestConsecutive(vector<int> &num) {
+		int max_seq = 0;
 		unordered_map<int, int> m;
 		for (size_t i=0; i<num.size(); ++i) {
-			if (m.count(num[i]) != 0)
+			int cur = num[i];
+			if (m.count(cur) != 0)
 				continue;
 
-			int left = num[i];
-			int right = num[i];
-			int sum = 1;
-			if (m.count(num[i]-1) > 0) {
-				sum += m[num[i]-1];
-				left -= m[num[i]-1];
-			}
-			if (m.count(num[i]+1) > 0) {
-				sum += m[num[i]+1];
-				right += m[num[i]+1];
-			}
+			int left_seq = 0;
+			if (m.count(cur-1) > 0)
+				left_seq = m[cur-1];
+			int right_seq = 0;
+			if (m.count(cur+1) > 0)
+				right_seq = m[cur+1];
 
-			seq = max(seq, sum);
-			m[left] = m[right] = sum;
+            int cur_seq = left_seq + right_seq + 1;
+			max_seq = max(max_seq, cur_seq);
+
+			m[cur] = cur_seq;
+			m[cur-left_seq]  = cur_seq;
+			m[cur+right_seq] = cur_seq;
 		}
-		return seq;
+		return max_seq;
 	}
 
-    int longestConsecutive(vector<int> &num) {
-        int seq = 0;
-        unordered_map<int, int> m;
-        for (size_t i=0; i<num.size(); ++i) {
-			if (m.count(num[i]) != 0)
+    int longestConsecutive_trick(vector<int> &num) {
+		int max_seq = 0;
+		unordered_map<int, int> m;
+		for (size_t i=0; i<num.size(); ++i) {
+			int cur = num[i];
+			if (m[cur] != 0)
 				continue;
 
-            m[num[i]] = num[i];
-
-			int x = num[i];
-            int up = num[i] - 1;
-			if (m.count(up)!=0 && m[up]<num[i]) {
-                x = m[up];
-                m[num[i]] = x;
-                m[x] = num[i];
-                if (x != up)
-                    m.erase(up);
-            }
-
-			int y = num[i];
-            int down = num[i] + 1;
-			if (m.count(down)!=0 && m[down]>num[i]) {
-                y = m[down];
-                m[x] = y;
-                m[y] = x;
-				// if (x != num[i])
-				// 	m.erase(num[i]);
-                if (y != down)
-                    m.erase(down);
-            }
-			seq = max(seq, y-x+1);
-			cout << num[i] << " " << seq << endl;
-			print(m);
+			max_seq = max(max_seq, m[cur-m[cur-1]]=m[cur+m[cur+1]]=m[cur]=m[cur-1]+m[cur+1]+1);
 		}
-        return seq;
+		return max_seq;
+	}
+
+    int longestConsecutive_orig(vector<int> &num) {
+        int max_seq = 0;
+        unordered_map<int, int> m;
+        for (size_t i=0; i<num.size(); ++i) {
+			int cur = num[i];
+			if (m.count(cur) != 0)
+				continue;
+
+			int left = cur;
+			int right = cur;
+
+            int up = cur - 1;
+			if (m.count(up)!=0 && m[up]<cur) {
+                left = m[up];
+				m.erase(up);
+				m.erase(left);
+            }
+
+            int down = cur + 1;
+			if (m.count(down)!=0 && m[down]>cur) {
+                right = m[down];
+				m.erase(right);
+				m.erase(down);
+            }
+
+			m[left] = right;
+			m[right] = left;
+			max_seq = max(max_seq, right-left+1);
+		}
+        return max_seq;
     }
+
+    int longestConsecutive_ninechapter(vector<int> &num) {
+		int max_seq = 0;
+		unordered_map<int, int> m;
+		for (size_t i=0; i<num.size(); ++i)
+			m[num[i]] = 0;
+
+		for (size_t i=0; i<num.size(); ++i) {
+			if (m[num[i]] == 1)
+				continue;
+
+			int cur_seq = 1;
+			int up = num[i] - 1;
+			while (m.count(up) > 0) {
+				cur_seq++;
+				m[up] = 1;
+				up--;
+			}
+
+			int down = num[i] + 1;
+			while (m.count(down) > 0) {
+				cur_seq++;
+				m[down] = 1;
+				down++;
+			}
+			max_seq = max(max_seq, cur_seq);
+		}
+		return max_seq;
+	}
 
     int longestConsecutive1(vector<int> &num) {
         int seq = 0;
@@ -115,7 +152,8 @@ public:
 
 int main(int argc, char *argv[])
 {
-	int arr[] = { 7,-2,9,9,1,9,8,-4,6,-6,-6,4,1,3,6,3,5,-2,3,4,-6,1,5,-9,6,1,2,-2,1 };
+	// int arr[] = { 7,-2,9,9,1,9,8,-4,6,-6,-6,4,1,3,6,3,5,-2,3,4,-6,1,5,-9,6,1,2,-2,1 };
+	int arr[] = { 1, 0 };
 	// int arr[] = { 100 };
 	int len = sizeof(arr) / sizeof(arr[0]);
 	vector<int> v(arr, arr+len);
